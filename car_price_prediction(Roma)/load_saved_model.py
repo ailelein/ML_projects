256537497aeca95fc03cb105d524578c66263308
Aliia <aliya.beiwenalieva@gmail.com>
2024-10-23 19:18:27 +0600

    updated

--- car_price_prediction(Roma)/load_saved_model.py
+++ car_price_prediction(Roma)/load_saved_model.py
@@ -1,6 +1,7 @@
 import pickle
 with open('saved_model.pkl', 'rb') as file:
     load_model = pickle.load(file)
+# it can be used array or lists to verify the model
 new_data = [[15000], [30000], [70000]]
 predicted_prices = load_model.predict(new_data)
 for mileage, price in zip(new_data, predicted_prices):
